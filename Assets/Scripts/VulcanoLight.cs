c0a163a3712fc2098a95d0c9aa872ccf5b2b0e3a
Gerd Schmidt <gerd@acagamics.de>
2016-04-02 00:05:22 +0200

    VulcaoTrees

---
+++ Assets/Scripts/VulcanoLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VulcanoLight : MonoBehaviour {
+
+    Light pointLight;
+
+    float rf = 0;
+
+	// Use this for initialization
+	void Start () {
+        pointLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Random.value>0.8)
+            rf += Random.value / 2;
+
+        pointLight.intensity = 6 + Mathf.Sin(Time.realtimeSinceStartup+rf)*3;
+
+
+    }
+}
