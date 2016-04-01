e0f28f482e2e4bba060e5644d1dfcd5edb19be2e
Maximilian Klockmann <maximilian.web@gmx.net>
2016-04-01 21:01:55 +0200

    Simple Pearl Spawner (Vulcan)

---
+++ Assets/Scripts/Pearl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Pearl : MonoBehaviour
+    {
+        #region methods
+
+        public float MaxDepth = -10;
+
+        private Rigidbody _rigidbody;
+
+        public Rigidbody Rigidbody
+        {
+            get
+            {
+                return _rigidbody = _rigidbody ?? GetComponent<Rigidbody>();
+            }
+        }
+
+        // Use this for initialization
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (transform.position.y < MaxDepth)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Vulcan.cs
@@ -0,0 +1,69 @@
+﻿namespace Assets.Scripts
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using UnityEngine;
+
+    public class Vulcan : MonoBehaviour
+    {
+        #region member vars
+        
+        public float SpawnRate = 2;
+        public Vector3 SpawnOffset;
+        public float SpawnRange = 1;
+        public float MaxSpawnSpeed = 5.0f;
+        public float MaxJumpSpeed = 10.0f;
+
+        public Pearl[] PearlTypes;
+
+        public List<Pearl> Pearls { get; private set; }
+
+        private float _timer;
+        private GameObject _pearlsObject;
+
+        #endregion
+
+        #region methods
+
+        // Use this for initialization
+        private void Start()
+        {
+            _timer = 0;
+            Pearls = new List<Pearl>();
+            _pearlsObject = new GameObject("Pearls");
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer > 1 / SpawnRate)
+            {
+                _timer = 0;
+                SpawnPearl();
+            }
+        }
+
+        private void SpawnPearl()
+        {
+            var rnd = Random.Range(0, PearlTypes.Length);
+            var pearl = Instantiate(PearlTypes[rnd]);
+
+            var spawnpos = Random.onUnitSphere * SpawnRange;
+            spawnpos.y = 0;
+
+            pearl.transform.position = transform.position + SpawnOffset + spawnpos;
+            pearl.transform.parent = _pearlsObject.transform;
+
+            Pearls.Add(pearl);
+
+            spawnpos.y = Random.Range(0, MaxJumpSpeed);
+
+            pearl.Rigidbody.AddForce(spawnpos * 100);
+        }
+
+        #endregion
+    }
+}
